97bf83347295137e089e6ef5a8e36e3d971e61a5
ranjan1990 <ranjan.rbc@gmail.com>
2016-05-12 02:09:15 +0530

    updated

--- Dense_idea/find_traj_matrix.py
+++ Dense_idea/find_traj_matrix.py
@@ -1,19 +1,37 @@
+import os
+
 file_name="/home/ranjan/Kaggle/dense_tracker_ECCV10/moseg/data/UCSD_Anomaly/UCSDped1/Test/Test001/BroxMalikResults/Tracks200.dat"
+
+#dir to store  individual cluster trajectory 
+dir1=file_name.split("/")[-3]
+try:
+        os.stat(dir1)
+except:
+        os.mkdir(dir1)       
+
 
 f=open(file_name,"r");
 f.readline()
 f.readline()
-
-while(True):
-    line=f.readline()
+D={}        #dictionary to get the count of number of tajectory
+line=f.readline()
+while(line!=""):
     line=line.split()
     t_label=int(line[0])
     t_no=int(line[1]) #numer of trajectory
+    if(D.has_key(t_label)):
+        D[t_label]= D[t_label]+1
+    else:
+        D[t_label]=1;
+
+    f1=open(dir1+"/"+str(t_label),"a")
     print t_label
     for i in range(t_no):
-        f.readline();
-    
+        line=f.readline();
+        f1.write(line);
 
-
+    f1.write("-------------")
+    f1.close()
+    line=f.readline()
 
 
