2ba48c2715fcb48656353bc6e792a9cbc9fe5a64
ranjan1990 <ranjan.rbc@gmail.com>
2016-05-12 19:41:07 +0530

    updated

--- Dense_idea/visual_traj_cluster.py
+++ Dense_idea/visual_traj_cluster.py
@@ -1,10 +1,15 @@
 import cv2
 import numpy as np
+
+src_path="/home/ranjan/Kaggle/dense_tracker_ECCV10/moseg/data/UCSD_Anomaly/UCSDped2/Test/Test001/"
+dest_path="/home/ranjan/Kaggle/dense_tracker_ECCV10/moseg/data/UCSD_Anomaly/UCSDped2/Test/Test001/"
+traj_file="Test001_cluster_traj2/0"
+
 #image show with normal window
 def imshow1(frame):
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.imshow("image",frame);
-    cv2.waitKey(10)
+    cv2.waitKey(50)
                
                
 #image show uning matplot
@@ -13,6 +18,7 @@ def imshow(img):
     plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
     plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
     plt.show()
+
 
 
 
@@ -35,21 +41,26 @@ def draw_circle(src_path,dest_path,line):
     img=cv2.imread(p);
     cv2.circle(img,(x,y),3, (0,0,255), -1)
     imshow1(img)
+    cv2.imwrite(traj_file+f_name,img);
+#find u,v,anglee
+def find_dx_dy_da(t1,t2):
+    frame_num=int(float(line[2]))+1 #+1 because frame number statrs from 0
+    x=int(float(line[0]))
+    y=int(float(line[1]))
+        
 
 
 
 
-
-src_path="/home/ranjan/Kaggle/dense_tracker_ECCV10/moseg/data/UCSD_Anomaly/UCSDped1/Test/Test001/"
-dest_path="/home/ranjan/Kaggle/dense_tracker_ECCV10/moseg/data/UCSD_Anomaly/UCSDped1/Test/Test001/"
-traj_file="Test001_cluster_traj/4"
 f=open(traj_file);
 line=f.readline();
 while(line!=""):
     while(line!="-------------\n"):
         line=line.split(" ");
         print line
-        draw_circle(src_path,dest_path,line)
+        img=draw_circle(src_path,dest_path,line)
+                
+
         line=f.readline();
 
     line=f.readline();
